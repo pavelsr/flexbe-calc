14853670ea3958a9863fe42f7b5509af06375c18
Pavel Serikov <pavelsr@cpan.org>
2022-03-04 03:29:34 +0300

    Added dynamic field

--- calc.js
+++ calc.js
@@ -8,69 +8,85 @@ var human_form_names = {
   need_installation: "Нужен монтаж"
 };
 
+$(document).ready(function() {
 
-$("#btnCalc").click(function() {
-  var form = $('#mainForm').serializeJSON();
-  console.log(form);
-
-  // var price_by_light = $('select#inputLightType option[value="' + form.light_type + '"]').data('price');
-  var price_by_light = $('select#inputLightType option:selected').data('price');
-  var price_face_color = $('select#inputFaceColor option:selected').data('price');
-  var price_side_color = $('select#inputSideColor option:selected').data('price');
-
-  var price_font = 0;
-
-  if ( form.is_non_standart_font) {
-    if ( form.symbol_height < 21 ) {
-      price_font = 8;
-    }
-    else if ( form.symbol_height >= 21 && form.symbol_height <= 40 ) {
-      price_font = 5;
-    }
-    else if ( form.symbol_height > 40 && form.symbol_height <= 60 ) {
-      price_font = 3;
+  $("#isLogo").click(function() {
+    var dyn_field_selector = "#dynLogoSquareInput";
+    if ( $(this).is(":checked") ) {
+      $(dyn_field_selector).show();
     }
     else {
-      console.log("Font is too large, need to calc manually!");
+      $(dyn_field_selector).hide();
     }
-  }
-
-  if (form.symbol_height < 12 ) {
-    form.symbol_height = 12;
-  }
-
-  var price_frame = (form.symbol_height/100)*1.5*form.symbol_count*1000;
-  var price_est = (price_by_light + price_face_color + price_side_color + price_font ) * form.symbol_height * form.symbol_count + price_frame;
-
-  // $("#calcResult").html('<h2>Предварительная стоимость вашей вывески: <b>' + price_est + '</b>₽<h2>');
-
-
-  var table = $('#calcResultDetailsTable');
-  table.empty();
-  var tbody = $('<tbody>');
-  $.each(human_form_names, function(key, human_field_name) {
-    var value = form[key];
-    console.log(value);
-
-    if ( value == 'on' ) {
-      value = "да "
-    }
-    if (typeof value === 'undefined') {
-      value = "нет"
-    }
-    tbody.append( $('<tr>').append( $('<td>').text(human_field_name) ).append( $('<td>').text(value) ) );
   });
 
-  if ( $('#deliveryModeCourier').is(':checked') ) {
-    tbody.append( $('<tr>').append( $('<td>').text('Доставка') ).append( $('<td>').text( $('#inputDeliveryDate').val() ) ) );
-  }
 
-  if ( $('#deliveryModePickup').is(':checked') ) {
-    tbody.append( $('<tr>').append( $('<td>').text('Самовывоз') ).append( $('<td>').text( $('#inputDeliveryDate').val() ) ) );
-  }
+  $("#btnCalc").click(function() {
+    var form = $('#mainForm').serializeJSON();
+    console.log(form);
 
-  tbody.append( $('<tr>').append( $('<td>').text('ИТОГО, руб') ).append( $('<td>').text(price_est) ) );
-  table.append( $('<table>').addClass('table table-bordered').append(tbody) );
+    // var price_by_light = $('select#inputLightType option[value="' + form.light_type + '"]').data('price');
+    var price_by_light = $('select#inputLightType option:selected').data('price');
+    var price_face_color = $('select#inputFaceColor option:selected').data('price');
+    var price_side_color = $('select#inputSideColor option:selected').data('price');
+
+    var price_font = 0;
+
+    if ( form.is_non_standart_font) {
+      if ( form.symbol_height < 21 ) {
+        price_font = 8;
+      }
+      else if ( form.symbol_height >= 21 && form.symbol_height <= 40 ) {
+        price_font = 5;
+      }
+      else if ( form.symbol_height > 40 && form.symbol_height <= 60 ) {
+        price_font = 3;
+      }
+      else {
+        console.log("Font is too large, need to calc manually!");
+      }
+    }
+
+    if (form.symbol_height < 12 ) {
+      form.symbol_height = 12;
+    }
+
+    var price_frame = (form.symbol_height/100)*1.5*form.symbol_count*1000;
+    var price_est = (price_by_light + price_face_color + price_side_color + price_font ) * form.symbol_height * form.symbol_count + price_frame;
+
+    // $("#calcResult").html('<h2>Предварительная стоимость вашей вывески: <b>' + price_est + '</b>₽<h2>');
 
 
+    var table = $('#calcResultDetailsTable');
+    table.empty();
+    var tbody = $('<tbody>');
+    $.each(human_form_names, function(key, human_field_name) {
+      var value = form[key];
+      console.log(value);
+
+      if ( value == 'on' ) {
+        value = "да "
+      }
+      if (typeof value === 'undefined') {
+        value = "нет"
+      }
+      tbody.append( $('<tr>').append( $('<td>').text(human_field_name) ).append( $('<td>').text(value) ) );
+    });
+
+    if ( $('#deliveryModeCourier').is(':checked') ) {
+      tbody.append( $('<tr>').append( $('<td>').text('Доставка') ).append( $('<td>').text( $('#inputDeliveryDate').val() ) ) );
+    }
+
+    if ( $('#deliveryModePickup').is(':checked') ) {
+      tbody.append( $('<tr>').append( $('<td>').text('Самовывоз') ).append( $('<td>').text( $('#inputDeliveryDate').val() ) ) );
+    }
+
+    tbody.append( $('<tr>').append( $('<td>').append('<b>ИТОГО, руб</b>') ).append( $('<td>').append( '<b>'+ price_est + '</b>' ) ) );
+    table.append( $('<table>').addClass('table table-bordered').append(tbody) );
+
+    if ( $('#isLogo').is(':checked') ) {
+      tbody.append( $('<tr>').append( $('<td>').text('Логотип, м2') ).append( $('<td>').text( $('input#inputLogoSquare').val() ) ) );
+      tbody.append( $('<tr>').append( $('<td>') ).append( $('<td>').text("Стоимость логотипа будет расчитана индивидуально") ) );
+    }
+  });
 });
